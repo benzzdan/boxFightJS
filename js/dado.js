e8bb5c876f810649555aeb005b083f3c5a92c457
Daniel Benson <bensondaniel664@gmail.com>
2016-08-22 13:17:56 -0500

    changed javascript

--- js/dado.js
+++ js/dado.js
@@ -1,19 +1,33 @@
+var but = document.getElementById('button1');
+var inNum = document.getElementById('inNum');
+
 //Funcion que nos va a mostrar el dado//
 function showDivs(){
  //elige numero random
  var randNum = Math.floor(Math.random()*6)+1;
  //agarra el div stage
-
  var stage = document.getElementById('stage');
  //cambia la imagen face1 por el numero que salio
  var img = document.getElementById('face1').getElementsByTagName('img');
  //cambia la cara de la imagen face1, ya que esta es la ultima imagen que muestra el dado cuando acaba la animacion
  img.first.src="images/dice"+randNum+".png"; //cambie la imagen a mostrar
- //Si el stage esta mostrandose cuando se le da click al boton, entonces, lo quita y quita lo que diga el banner
+
+
+
+
+
+
+//Si el stage esta mostrandose cuando se le da click al boton, entonces, lo quita y quita lo que diga el banner
  if (stage.style.display === "flex"){
-     stage.style.display="none";
-     document.getElementById('banner').innerHTML="";
+     stage.style.display="none"; //quita el dado
+     document.getElementById('banner').innerHTML="";//quita el banner
+     if(inNum.value.length !== 0){
+       //setTimeout(stage.style.display="flex", 3000);
+       setTimeout(showDice, 1000);
+       setTimeout(checkInput, 4000);
      }
+   }
+
   //en caso de que no estre mostrandose el dado, lo va a mostrar y va a mostrar el banner
  else{
     stage.style.display="flex";
@@ -24,65 +38,36 @@ function showDivs(){
      function checkInput() {
        var n = document.getElementById('inNum');
        //sacar el valor y compararlo con el de randNum y determinar si ganaste
-       if(parseInt(n.innerHTML) === randNum){
-         document.getElementById('banner').innerHTML="<h2 id='win'>GANASTE!</h2>";
+       if(parseInt(n.value) === randNum){
+         document.getElementById('banner').innerHTML="<h2 id='win'>YOU WIN!</h2>";
        }
        else {
-         document.getElementById('banner').innerHTML="<h2 id='looser'>PERDISTE!</h2>";
+         document.getElementById('banner').innerHTML="<h2 id='looser'>YOU LOOSE!</h2>";
        }
 
      }
-     //Funcion que muestra el banner de ganador o perdedor
-     function Banners(){
-            if(randNum === 6){
-              document.getElementById('banner').innerHTML="<h2 id='mega'>MEGA GANASTE!</h2>";
-            }
-            else if(randNum % 2 === 0) {
-              document.getElementById('banner').innerHTML="<h2 id='win'>GANASTE!</h2>";
-            }
-            else{
-              document.getElementById('banner').innerHTML="<h2 id='looser'>PERDISTE!</h2>";
-            }
+
+          changeButtonLayout();
+           //sets the button to play again if it said play before
+          setFocus(); //sets the focus and cursor on the input tag
+
+} ////END ShowDivs
+
+        function changeButtonLayout() {
+          if(inNum.value.length === 0){
+            but.innerHTML = "PLAY";
+          }else {
+            but.innerHTML = "PLAY AGAIN!";
           }
-          playAgain();
-          setFocus();
+
+
+          //si hay valor en input, y si se le da click en el boton play again
+          // que corra de nuevo el proceso del numero random y el dado si no
+          //que regrese al placeholder y tome el valor de input de nuevo ya que el user haya
+          //ingresado el valor.
+
         }
 
-        function playAgain() {
-          var but = document.getElementById('button1')
-          if(but.innerHTML === "PLAY"){
-            but.innerHTML = "PLAY AGAIN!"
-          }
-          else {
-            but.innerHTML = "PLAY"
-            var inNum = document.getElementById('inNum');
-            inNum.value="";
-            //setCursor();
-          }
-        }
-
-        //autofocus
-
-        //Blinker function
-/* //////////nice function that makes placeholder blink its content///////////
-        function blinker() {
-          //gets the placeholder of input
-          if($('input[type=text]').attr('placeholder'))
-          {
-            //sets the placeholder empty if it has something
-            $('input[type=text]').attr('placeholder', '');
-          }
-          else
-          {
-            $('input[type=text]').attr('placeholder', 'Ingresa un número');
-
-          }
-
-          setTimeout(blinker, 1000);
-          setFocus();
-          setCursor();
-        }
-        */
 
 //////Cuando se ejecuta el documento se carga este script //////
         function setFocus() {
@@ -108,4 +93,24 @@ function showDivs(){
         if(input[0].value){
           input.css("text-align", "center");
         }
+        else {
+          input.css("text-align", "right");
+          //button changes to 'play' only when user erases the value from the input
+          changeButtonLayout();
+        }
+
+      }
+
+      function playSameNum() {
+        //Play again if, there is a value on input and you hit the button1
+        var inNum = document.getElementById('inNum');
+        if (inNum.value) {
+          stage.style.display="none"; //quita el dado
+          document.getElementById('banner').innerHTML=""; //quita el banner
+          setTimeout(showDivs, 3000);
+        }
+      }
+
+      function showDice() {
+        stage.style.display="flex";
       }
